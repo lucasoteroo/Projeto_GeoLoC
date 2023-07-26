2523c8725df997fc80d78e38e375a1bba5c4355c
Lucas Sotero <105369580+lucasoteroo@users.noreply.github.com>
2023-07-26 11:57:51 -0300

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -1,11 +1,10 @@
-import requests
 from flask import Flask, request, render_template
 import telebot
-import json
+import os
 
 
 # Token de acesso do bot do Telegram
-token = '6148425947:AAFjBOwCHGFjc_pvR_Bjz1nCLwZ_euyyPgA'
+token = '6103231923:AAH1sxXKyQMZrwbrjT9VsL5coUK7OD24qT4'
 
 # Cria uma nova instância do bot
 bot = telebot.TeleBot(token)
@@ -25,11 +24,6 @@ def style():
 def script():
     return app.send_static_file('script.js')
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    # Processa a atualização recebida
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "OK"
 
 # Rota para o comando /start
 @bot.message_handler(commands=['start'])
@@ -43,30 +37,17 @@ def enviar_distancia():
     localizacoes = request.json['localizacoes']
     chat_id = request.json['chat_id']  # Obtém o chat ID do JSON recebido
 
-    mensagem = "Distâncias:\n"
+    mensagem = "Você não está próximo de nenhuma localização"
     for localizacao in localizacoes:
         distancia = localizacao['distancia']
         nome = localizacao['name']
-        mensagem += f"Sua localizão atual é: {nome}\n"
+        mensagem += f"Você está a {distancia} km de {nome}\n"
 
     # Envie a mensagem para o bot do Telegram usando a API do Telegram
     bot.send_message(chat_id, mensagem)
 
     return "OK"
-
-
-def set_telegram_webhook(url):
-    webhook_url = f'https://api.telegram.org/bot{token}/setWebhook?url={url}/webhook'
-    response = requests.get(webhook_url)
-    if response.ok:
-        print('Webhook definido com sucesso')
-    else:
-        print('Falha ao definir o webhook')
-
 if __name__ == '__main__':
-    # Define a URL do webhook para receber as atualizações do bot do Telegram
-    ngrok_url = 'https://05bd-179-127-33-48.ngrok-free.app'  # Substitua pela URL do ngrok
-    set_telegram_webhook(ngrok_url)
+    port = int(os.environ.get("PORT", 5000))  # Obtém a porta do ambiente do Heroku
+    app.run(host='0.0.0.0', port=port)
 
-    # Inicia o servidor web Flask
-    app.run(host='0.0.0.0', port=5000)
